d69eff990ba56028d2f57c688dbd9d7605504895
Thomas Wolgast Rørbech <thwr@itu.dk>
2022-03-04 16:45:08 +0100

    Copy message and user structs from main repo
    To ease the printing

--- main.go
+++ main.go
@@ -94,3 +94,18 @@ func main() {
 		fmt.Println(response)
 	}
 }
+
+// straight copy pasta from Minitwit.models
+type User struct {
+	UserId       int64
+	Username     string
+	Email        string
+	PasswordHash string
+}
+
+type Message struct {
+	Author  User
+	PubDate int64  // The publish timestamp as UNIX
+	Text    string // The message itself
+	Flagged bool
+}
